f72905aeb576dfceacb60bfd25a4a9b838d5c32e
BrianPhiri <brianphiri.9523@gmail.com>
2017-05-27 21:55:08 +0300

    added create, list and delete functions

--- server/main.go
+++ server/main.go
@@ -1,11 +1,43 @@
 package main
 
+import(
+	pb "github.com/BrianPhiri/grpc_go_intro/api"
+)
+
 const (
-	port: ":1234"
+	port = ":1234"
 )
 
 type server struct{
-	store.database.Database
+	store database.Database
 }
 
-fun (s *server) createTodo()
+func (s *server) createTodo(ctx context.Context, in *pb.createTodoRequest) (*pb.createTodoResponse, error){
+	todo, err := s.store.createTodo(in.Todo)
+	if err != nil{
+		return nil, err
+	}
+	return &pb.createTodoResponse{
+		Todo: todo
+	}, nil
+}
+
+func (s *server) listTodo(ctx contex.Context, in *pb.listTodoRequest) (*pb.listTodoResponse, error){
+	todo, err := s.store.listTodo()
+	if(err !=nil){
+		return nil, err
+	}
+	return &pb.listTodoResponse{
+		Todo: todo
+	}, nil
+}
+
+func (s *server) deleteTodo(ctx context.Context, in *pb.deleteTodoRequest) (*pb.deleteTodoResponse, error){
+	todo, err := s.store.deleteTodo(in.id)
+	if(err != nil){
+		return nil, err
+	}
+	return &pb.deleteTodoResponse{
+		Todo: todo
+	}, nil
+}
